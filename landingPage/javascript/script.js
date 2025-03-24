8399827435e98d721d5df1a136a5b3177784c03f
Matheus Araújo Akiyoshi Loureiro <matheusaaloureiro@gmail.com>
2025-03-24 12:31:17 -0300

    a
    a

--- landingPage/javascript/script.js
+++ landingPage/javascript/script.js
@@ -1,5 +1,6 @@
 $(function() {
     $('#mobile_btn').on('click', function() {
         $('#mobile_menu').toggleClass('active');
+        $('#mobile_btn').find('i').toggleClass('fa-x');
     });
 });
